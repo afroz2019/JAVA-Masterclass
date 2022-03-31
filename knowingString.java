2d873670b07efe5d701777eeea24af6125381258
AfrozMondal <52920488+afroz2019@users.noreply.github.com>
2022-03-31 20:15:07 +0530

    Add files via upload

---
+++ knowingString.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class knowingString {
+
+    public static void main(String[] args) {
+
+        String myString = "This is a string";
+        System.out.println("My string is =" + myString);
+        myString = myString + ", this more." ;
+        System.out.println("My string is =" + myString);
+    }
+}
