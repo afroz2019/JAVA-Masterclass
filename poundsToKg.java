292f723c82e51f5670990c93bf3f5bd8666aeb81
AfrozMondal <52920488+afroz2019@users.noreply.github.com>
2022-03-31 20:05:28 +0530

    Add files via upload

---
+++ poundsToKg.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class poundsToKg {
+
+    public static void main(String[] args) {
+
+        int pounds = 200;
+        double kg = (pounds*0.45359237);
+        System.out.println("200 pound is =" + kg);
+    }
+}
