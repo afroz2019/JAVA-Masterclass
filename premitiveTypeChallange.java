22d08244a0cbf4393d6ed27b9b971b1f89bde21e
AfrozMondal <52920488+afroz2019@users.noreply.github.com>
2022-03-31 19:52:29 +0530

    Add files via upload

---
+++ premitiveTypeChallange.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class premitiveTypeChallange {
+
+    public static void main(String[] args) {
+        //declear byte variable
+	byte byteChallange = 10;
+	    //declear short variable
+    short shortChallange = 20;
+        //declear int variable
+    int intChallange = 50;
+        //declear long variable
+    long myLongTotal =(50000 + 10* (byteChallange + shortChallange + intChallange));
+
+        System.out.println("MY TOTAL LONG = " +myLongTotal);
+    }
+}
