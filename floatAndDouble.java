fa202ec8e34433c6c0b2a73a5493606fd79b2943
AfrozMondal <52920488+afroz2019@users.noreply.github.com>
2022-03-31 19:55:37 +0530

    Add files via upload

---
+++ floatAndDouble.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class floatAndDouble {
+
+    public static void main(String[] args) {
+	        //float Data Type
+        float myfloatMaxValue = Float.MAX_VALUE;
+        float myfloatMinValue = Float.MIN_VALUE;
+
+        System.out.println("My max float value =" +myfloatMaxValue);
+        System.out.println("My min float value =" +myfloatMinValue);
+
+            //double Data Type
+        double mydoubleMaxValue = Double.MAX_VALUE;
+        double mydoubleMinValue = Double.MIN_VALUE;
+
+        System.out.println("My max double value =" +mydoubleMaxValue);
+        System.out.println("My min double value =" +mydoubleMinValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5f;
+        double myDoubleValue = 5d;
+        System.out.println("print float value = " +myFloatValue);
+        System.out.println("print double value = " +myDoubleValue);
+
+
+
+
+
+    }
+}
